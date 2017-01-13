f6aaf0b818d1a5961d402fb53f1508804ac194fe
Javier Pallarés <javpalsaa@alum.us.es>
2017-01-13 02:10:58 +0100

    Configurado el proyecto con plugin y dependencia log4j siguiendo tutorial de la wiki

--- src/main/java/es/egc/app/App.java
+++ src/main/java/es/egc/app/App.java
@@ -1,13 +1,22 @@
 package es.egc.app;
 
+import org.apache.log4j.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	
+	static Logger log=Logger.getLogger(App.class);
+	
+   static int getVal(){
+	return 1;
+	}
+   
+   public static void main(String[] args ){
+	   System.out.println("Hello world");
+	   log.info("Returning 1");
+   }
 }
